13a9dc417d26980148700a979acf6fb64a598d16
lroehrich <s131740@student.dhbw-mannheim.de>
2016-05-19 12:28:35 +0200

    initialCommit

---
+++ SmartInsurance/www/js/app.js
@@ -0,0 +1,119 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+  
+  .state('app.uebersicht', {
+    url: '/uebersicht',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/uebersicht.html'
+      }
+    }
+  })
+
+  
+  .state('app.profil', {
+    url: '/profil',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profil.html'
+      }
+    }
+  })
+
+  
+  .state('app.insurance_add', {
+    url: '/insurance_add',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/insurance_add.html'
+      }
+    }
+  })
+
+
+  .state('app.investment_search', {
+      url: '/investment_search',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/investment_search.html'
+        }
+      }
+    })
+   .state('app.faq', {
+      url: '/faq',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/faq.html'
+        }
+      }
+    })
+   .state('app.impressum', {
+      url: '/impressum',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/impressum.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/uebersicht');
+});
