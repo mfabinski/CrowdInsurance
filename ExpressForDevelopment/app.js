2744f4fc84b35efdd03a419583ae4e4ef45eb3d1
dhentschel06 <s130861@student.dhbw-mannheim.de>
2016-05-20 12:58:09 +0200

    first database calls

--- ExpressForDevelopment/app.js
+++ ExpressForDevelopment/app.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var database = require('database.js');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  database.gib(function(result){
+    res.send(result);
+  })
 });
+
+app.get('/sf', function(req, res, next){database.getSchadensfaelle(req, res, next);})
+
+app.get('/versicherungen', function(req, res, next){database.getVersicherungen(req, res, next);})
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
---
+++ ExpressForDevelopment/guideHowToStartTheServer.md
@@ -0,0 +1,7 @@
+1. Install nodejs
+1. Open command prompt and navigate to this folder
+1. Type: 'node app.js' and press enter
+1. A local nodejs server will start on ur localhost port 3000
+1. Click [here](http://localhost:3000) to open ur browser and see the result
+
+Have fun :-)
