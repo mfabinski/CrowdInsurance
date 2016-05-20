70303b4aa4920e15fd84e021b1c8b7291454abd7
dhentschel06 <s130861@student.dhbw-mannheim.de>
2016-05-21 01:52:06 +0200

    logic angelegt, database in tdg umbenannt, erste services definiert

--- ExpressForDevelopment/app.js
+++ ExpressForDevelopment/app.js
@@ -1,18 +1,15 @@
 var express = require('express');
-var database = require('database.js');
+var tdg = require('tabledatagateway.js');
+var logic = require('logic.js');
 var app = express();
 
 app.get('/', function (req, res) {
-  database.gib(function(result){
-    res.send(result);
-  })
+  res.send("Der Sever laeuft! :-)");
 });
 
-app.get('/sf', database.getSchadensfaelle)
+app.get('/api/versicherung', logic.getVersicherungOf)
 
-app.get('/versicherungen', database.getVersicherungen)
-
-app.get('/versicherung', database.getVersicherung)
+app.get('/api/versicherung/:versicherungID', logic.getVersicherung)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
