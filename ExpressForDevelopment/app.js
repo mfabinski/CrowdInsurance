0e26d509145e65270bae482b17902e10959ae6d6
matthias <matthias.fab@gmail.com>
2016-05-21 17:28:25 +0200

    Scheduling

--- ExpressForDevelopment/app.js
+++ ExpressForDevelopment/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var tdg = require('tabledatagateway.js');
 var logic = require('logic.js');
 var bodyParser = require('body-parser');
+var schedule = require('node-schedule');
+
 var app = express();
 app.use( bodyParser.json() );
 
@@ -26,6 +28,19 @@ app.get('/api/versicherung/:versicherungID/person', logic.getInvestorenVonVersic
 app.get('/api/versicherung/:versicherungID/invest', logic.calculateSumOfInvestVersicherung)
 
 app.post('/api/versicherung', logic.erstelleVersicherung)
+
+/*
+*    *    *    *    *    *
+┬    ┬    ┬    ┬    ┬    ┬
+│    │    │    │    │    |
+│    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+│    │    │    │    └───── month (1 - 12)
+│    │    │    └────────── day of month (1 - 31)
+│    │    └─────────────── hour (0 - 23)
+│    └──────────────────── minute (0 - 59)
+└───────────────────────── second (0 - 59, OPTIONAL)
+*/
+var job = schedule.scheduleJob('*/10 * * * * *', logic.periodicSchedule); // run every 10 seconds
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
