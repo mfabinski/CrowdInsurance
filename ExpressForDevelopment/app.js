8d87334ba50e9741822e52a271a2a6a44b2f56bf
dhentschel06 <s130861@student.dhbw-mannheim.de>
2016-05-21 11:46:19 +0200

    bodyparser hinzugefuegt, einge selects und ein insert service definiert

--- ExpressForDevelopment/app.js
+++ ExpressForDevelopment/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var tdg = require('tabledatagateway.js');
 var logic = require('logic.js');
+var bodyParser = require('body-parser');
 var app = express();
+app.use( bodyParser.json() );
 
 app.get('/', function (req, res) {
   res.send("Der Sever laeuft! :-)");
@@ -10,6 +12,19 @@ app.get('/', function (req, res) {
 app.get('/api/versicherung', logic.getVersicherungOf)
 
 app.get('/api/versicherung/:versicherungID', logic.getVersicherung)
+
+app.post('/api/investieren', logic.erstelleInvestition)
+
+app.get('/api/investition', logic.getInvestitionOf)
+
+app.get('/api/investitionVers/:versicherungID', logic.listOfInvestitionen)
+
+app.get('/api/investition/:investitionID', logic.getInvestition)
+
+app.get('/api/versicherung/:versicherungID/person', logic.getInvestorenVonVersicherung)
+
+app.get('/api/versicherung/:versicherungID/invest', logic.calculateSumOfInvestVersicherung)
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
