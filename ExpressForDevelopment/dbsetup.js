3992eee0dd67a228fa4543f1bfca8d6c7b380d38
matthias <matthias.fab@gmail.com>
2016-05-25 13:42:22 +0200

    ci

---
+++ ExpressForDevelopment/dbsetup.js
@@ -0,0 +1,19 @@
+var pg = require('pg');
+var fs = require('fs');
+
+var sql = fs.readFileSync('setup.sql').toString();
+
+pg.connect('postgres://smartinsurance:123456@localhost/smartbackendevelopment', function(err, client, done){
+    if(err){
+        console.log('error: ', err);
+        process.exit(1);
+    }
+    client.query(sql, function(err, result){
+        done();
+        if(err){
+            console.log('error: ', err);
+            process.exit(1);
+        }
+        process.exit(0);
+    });
+});
