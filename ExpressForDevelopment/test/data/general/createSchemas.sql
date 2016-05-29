86ffb90e7ed91d9f80a64bd25404c07e64707291
matthias <matthias.fab@gmail.com>
2016-05-29 19:22:46 +0200

    next Test

--- ExpressForDevelopment/test/data/general/createSchemas.sql
+++ ExpressForDevelopment/test/data/general/createSchemas.sql
@@ -12,6 +12,7 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
+set lc_monetary="de_DE(at)euro";
 
 --
 -- Name: smartbackend; Type: SCHEMA; Schema: -; Owner: -
@@ -761,4 +762,3 @@ ALTER TABLE ONLY "Zahlungsstrom"
 --
 -- PostgreSQL database dump complete
 --
-
