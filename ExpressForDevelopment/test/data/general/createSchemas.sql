e553bb80b73f0ef14f9b69af16f1d8b1e15d7673
matthias <matthias.fab@gmail.com>
2016-05-29 21:29:56 +0200

    Tests testen

--- ExpressForDevelopment/test/data/general/createSchemas.sql
+++ ExpressForDevelopment/test/data/general/createSchemas.sql
@@ -12,7 +12,7 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
-set lc_monetary="de_DE(at)euro";
+set lc_monetary = 'de_DE.UTF-8';
 
 --
 -- Name: smartbackend; Type: SCHEMA; Schema: -; Owner: -
