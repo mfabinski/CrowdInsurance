3d457ef9e53fa8a4ab648647cedb4c8b637c15a0
dhentschel06 <s130861@student.dhbw-mannheim.de>
2016-05-29 14:00:44 +0200

    Installierung von Winston.js sowie einfügen von logs statt einfachen Konsolen eingaben. Logs werden im ordner ExpressForDevelopment/logs abgelegt

--- ExpressForDevelopment/app.js
+++ ExpressForDevelopment/app.js
@@ -3,6 +3,9 @@ var tdg = require('tabledatagateway.js');
 var ts = require('transactionscript.js');
 var bodyParser = require('body-parser');
 var schedule = require('node-schedule');
+var logger = require('logger.js');
+
+logger.clearLogFiles();
 
 var app = express();
 app.use( bodyParser.json() );
@@ -11,27 +14,27 @@ app.get('/', function (req, res) {
   res.send("Der Sever laeuft! :-)");
 });
 
-app.get('/api/smartinsurance/versicherung', ts.getVersicherungOf)
+app.get('/api/smartinsurance/versicherung', ts.getVersicherungOf);
 
-app.get('/api/smartinsurance/versicherung/:versicherungID', ts.getVersicherung)
+app.get('/api/smartinsurance/versicherung/:versicherungID', ts.getVersicherung);
 
-app.post('/api/smartinsurance/investieren', ts.erstelleInvestition)
+app.post('/api/smartinsurance/investieren', ts.erstelleInvestition);
 
-app.get('/api/smartinsurance/investition', ts.getInvestitionOf)
+app.get('/api/smartinsurance/investition', ts.getInvestitionOf);
 
-app.get('/api/smartinsurance/investitionVers/:versicherungID', ts.listOfInvestitionen)
+app.get('/api/smartinsurance/investitionVers/:versicherungID', ts.listOfInvestitionen);
 
-app.get('/api/smartinsurance/investition/:investitionID', ts.getInvestition)
+app.get('/api/smartinsurance/investition/:investitionID', ts.getInvestition);
 
-app.get('/api/smartinsurance/versicherung/:versicherungID/person', ts.getInvestorenVonVersicherung)
+app.get('/api/smartinsurance/versicherung/:versicherungID/person', ts.getInvestorenVonVersicherung);
 
-app.get('/api/smartinsurance/versicherung/:versicherungID/invest', ts.calculateSumOfInvestVersicherung)
+app.get('/api/smartinsurance/versicherung/:versicherungID/invest', ts.calculateSumOfInvestVersicherung);
 
-app.post('/api/smartinsurance/versicherung', ts.erstelleVersicherung)
+app.post('/api/smartinsurance/versicherung', ts.erstelleVersicherung);
 
-app.post('/api/smartinsurance/versicherung/:versicherungID/kuendigen', ts.versicherungKuendigungEinreichen)
+app.post('/api/smartinsurance/versicherung/:versicherungID/kuendigen', ts.versicherungKuendigungEinreichen);
 
-app.post('/api/smartinsurance/investition/:investitionID/kuendigen', ts.investitionKuendigungEinreichen)
+app.post('/api/smartinsurance/investition/:investitionID/kuendigen', ts.investitionKuendigungEinreichen);
 
 /*
 *    *    *    *    *    *
@@ -45,9 +48,15 @@ app.post('/api/smartinsurance/investition/:investitionID/kuendigen', ts.investit
 └───────────────────────── second (0 - 59, OPTIONAL)
 */
 if (process.argv[2] == undefined){ //wenn kein testmodus aktiv ist
-  var job = schedule.scheduleJob('0 */10 * * * *', ts.periodicSchedule); // run every 10 minutes
+  var period = '0 */1 * * * *';
+  var job = schedule.scheduleJob(period, ts.periodicSchedule); // run every 10 minutes
+  logger.consoleInfo('Periodischen Job gestartet - ' + period);
 }
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  if (process.argv[2] == undefined){
+    logger.consoleInfo('App hört auf port 3000 - Produktier Modus');
+  } else{
+    logger.consoleInfo('App hört auf port 3000 - Test Modus');
+  }
 });
