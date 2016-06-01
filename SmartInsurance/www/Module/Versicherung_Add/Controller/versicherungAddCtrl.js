c092b7b4743485c19dab251d56eb3c32680c8d0c
lroehrich <s131740@student.dhbw-mannheim.de>
2016-06-01 11:01:02 +0200

    Validierung von Versicherung anlegen

--- SmartInsurance/www/Module/Versicherung_Add/Controller/versicherungAddCtrl.js
+++ SmartInsurance/www/Module/Versicherung_Add/Controller/versicherungAddCtrl.js
@@ -1,25 +1,60 @@
 appController.controller('versicherungAddCtrl',function($scope, $http, moneyParser, moneyFormatter){
    
     $scope.selected_category ="";
-    $scope.insurance_category = ["Auto", "Haus", "Computer"];
+    $scope.insurance_category = ["Auto", "Haus", "Computer", "Smartphone", "Sonstiges"];
     
     $scope.versicherung = {
         name:"",
 //      kategorie: "",
         versicherungshoehe: "", 
-//      wert: "",
+//        wert: "",
         beitrag: "", 
         beschreibung: ""
     }
-   
     
-    $scope.versicherungAdd = function() {
+
+    
+  
+    $scope.isInvalid = function(field){
+
+        return field.$invalid && field.$touched;
+     };
+    
+    $scope.isNull = function(field){
+
+        return (field.$modelValue == "0,00 €");
+    };
+    
+    $scope.isNaN = function(field) {
+        
+        if(angular.isDefined(field.$modelValue)){
+            field.$modelValue = moneyFormatter.formatMoney(moneyParser.moneyparsen(field.$modelValue)); 
+
+            if(field.$modelValue == "0,00 €"){
+                return true && field.$touched;
+            } 
+        }
+        return false;     
+    };
+
+
+
+    
+    $scope.versicherungAdd = function(form) {
     /*    $http.post('http://localhost:3000/api/smartinsurance/versicherung', $scope.versicherung).then(function(data) {
             $scope.msg = 'Erfolgreich abgeschlossen.';
-        });  */
+        });  */    
+        
         $scope.versicherung.versicherungshoehe = moneyFormatter.formatMoney(moneyParser.moneyparsen($scope.versicherung.versicherungshoehe));
         $scope.versicherung.beitrag = moneyFormatter.formatMoney(moneyParser.moneyparsen($scope.versicherung.beitrag));
-        $scope.msg = 'Data sent: '+ JSON.stringify($scope.versicherung);
-    }
+ //     $scope.versicherung.wert = moneyFormatter.formatMoney(moneyParser.moneyparsen($scope.versicherung.wert));
+        
+   
+        if (form.$dirty   && form.$valid){
+            $scope.msg = 'Data sent: '+ JSON.stringify($scope.versicherung);
+         } else {
+            $scope.msg = 'Die Daten konnten nicht gesendet werden';
+        }
+    };
 });
 
