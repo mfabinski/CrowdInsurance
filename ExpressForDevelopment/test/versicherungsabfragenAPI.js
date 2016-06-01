50c2b3a64c0ccd7dc51cb37bf0f00252d9dc71e0
matthias <matthias.fab@gmail.com>
2016-06-01 12:03:15 +0200

    Adjust Test to new RequestResponseBody

--- ExpressForDevelopment/test/versicherungsabfragenAPI.js
+++ ExpressForDevelopment/test/versicherungsabfragenAPI.js
@@ -60,10 +60,10 @@ describe("Versicherung abfragen", function(){
       expect(response.statusCode).to.equal(200);
       var responseObject = JSON.parse(body);
       logger.consoleInfo(body);
-      expect(responseObject).to.have.property('data').with.length(1);
-      expect(responseObject.data[0]).to.have.property('abschlussZeitpunkt');
-      expect(responseObject.data[0]).to.have.property('beitrag').to.equal(expectedResponse.data[0].beitrag);
-      expect(responseObject.data[0]).to.have.property('istGekuendigt').to.equal(expectedResponse.data[0].istGekuendigt);
+      expect(responseObject).to.have.length(1);
+      expect(responseObject[0]).to.have.property('abschlussZeitpunkt');
+      expect(responseObject[0]).to.have.property('beitrag').to.equal(expectedResponse.data[0].beitrag);
+      expect(responseObject[0]).to.have.property('istGekuendigt').to.equal(expectedResponse.data[0].istGekuendigt);
 //      expect(responseObject.data[0]).to.have.property('wirdGekuendigt').to.equal(expectedResponse.data[0].wirdGekuendigt);
       // expect(JSON.parse(body)).to.deep.equal(expectedResponse);
       done();
