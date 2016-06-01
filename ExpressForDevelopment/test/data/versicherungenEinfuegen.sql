a91e5da9e7518b8dbc07f0c1f7439573a95b471a
dhentschel06 <s130861@student.dhbw-mannheim.de>
2016-06-01 16:30:50 +0200

    Kategorie zu den Testfällen hinzugefügt

--- ExpressForDevelopment/test/data/versicherungenEinfuegen.sql
+++ ExpressForDevelopment/test/data/versicherungenEinfuegen.sql
@@ -1,15 +1,15 @@
 INSERT INTO smartbackend."user"(
             id, name, prename, email)
     VALUES ('bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Hardwood', 'Lance', 'hardwood@hotmail.com');
-INSERT INTO smartinsurance."Versicherung"(name, versicherungshoehe, beitrag, beschreibung, "personID")
+INSERT INTO smartinsurance."Versicherung"(name, versicherungshoehe, beitrag, beschreibung, "personID", kategorie)
     VALUES
-      ('Audi A8','5.000,00 €','100,00 €','schoenes auto','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('VW Polo','1.000,00 €','40,00 €','toller wagen','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('Renault 206','500,00 €','8,00 €','schnelles auto','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('Reihenhaus in Mannheim','10.000,00 €','100,00 €','schoene gegend','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('Villa in Bel Air','200.000,00 €','1.000,00 €','pompoes','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('Wohnung in Kreuzberg','3.000,00 €','10,00 €','heruntergekommen','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('Rolex Daytona','500,00 €','5,00 €','geht nicht kaputt','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('Breitling Superocean','1.000,00 €','10,00 €','superlanges Megaarmband dass mehr Zeichen hat als du ueberhaupt schreiben kannst','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('Boot','250.000,00 €','1.000,00 €','Guter Rumpf','bfa73b02-21f7-11e6-b56d-6ffe69564a95'),
-      ('Tuer zu meinem Buero','20,00 €','1,00 €','Tuechtige Tuer','bfa73b02-21f7-11e6-b56d-6ffe69564a95');
+      ('Audi A8','5.000,00 €','100,00 €','schoenes auto','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Auto'),
+      ('VW Polo','1.000,00 €','40,00 €','toller wagen','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Auto'),
+      ('Renault 206','500,00 €','8,00 €','schnelles auto','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Auto'),
+      ('Reihenhaus in Mannheim','10.000,00 €','100,00 €','schoene gegend','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Haus'),
+      ('Villa in Bel Air','200.000,00 €','1.000,00 €','pompoes','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Haus'),
+      ('Wohnung in Kreuzberg','3.000,00 €','10,00 €','heruntergekommen','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Haus'),
+      ('Rolex Daytona','500,00 €','5,00 €','geht nicht kaputt','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Uhr'),
+      ('Breitling Superocean','1.000,00 €','10,00 €','superlanges Megaarmband dass mehr Zeichen hat als du ueberhaupt schreiben kannst','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Uhr'),
+      ('Boot','250.000,00 €','1.000,00 €','Guter Rumpf','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Schiff'),
+      ('Tuer zu meinem Buero','20,00 €','1,00 €','Tuechtige Tuer','bfa73b02-21f7-11e6-b56d-6ffe69564a95', 'Möbel');
