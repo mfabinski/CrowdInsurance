c52fba192e233ac64aa5ccb9c00705a0ecfc0a43
dhentschel06 <s130861@student.dhbw-mannheim.de>
2016-06-02 13:48:45 +0200

    AngularJS Post, Put, Delete, Update, etc. sollte jetzt funktionieren ;)

--- ExpressForDevelopment/app.js
+++ ExpressForDevelopment/app.js
@@ -15,6 +15,21 @@ app.get('/', function (req, res) {
   res.send("Der Sever laeuft! :-)");
 });
 
+app.get('/', function (req, res) {
+  res.send("Der Sever laeuft! :-)");
+});
+
+app.all('*', function(req, res, next) {
+    // add details of what is allowed in HTTP request headers to the response headers
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', false);
+    res.header('Access-Control-Max-Age', '86400');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    // the next() function continues execution and will move onto the requested URL/URI
+    next();
+});
+
 app.get('/api/smartinsurance/versicherung', ts.getVersicherungOf);
 
 app.get('/api/smartinsurance/versicherung/:versicherungID', [validate.versicherungID, ts.getVersicherung]);
