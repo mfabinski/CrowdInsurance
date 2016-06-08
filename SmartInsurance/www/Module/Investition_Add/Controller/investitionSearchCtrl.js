2b882e17bb37825f5dfa8bd791566f8b9890fc83
ezuyeva <lisa.zuyeva@web.de>
2016-06-09 01:44:43 +0200

    Suche mit Filterung und Sortierung fertig

--- SmartInsurance/www/Module/Investition_Add/Controller/investitionSearchCtrl.js
+++ SmartInsurance/www/Module/Investition_Add/Controller/investitionSearchCtrl.js
@@ -10,22 +10,21 @@ appController.controller('investitionSearchCtrl',function($scope, $http, apiendp
     $http.get(apiendpoint.url + '/api/smartinsurance/kategorien').success(function(response) {
 
       var kat = [];
-      kat.push({value: "Keine"});
+      //kat.push({value: "Keine"});
 
          for (var i = 0; i < response.length; i++) {
-           kat.push({value: response[i]});
+           kat.push({text: response[i]});
          }
       $scope.kategorien = kat;
 
     });
 
     $scope.kategorienFilter = {
-      value: ''
+      text: "Alle"
   };
 
 
   $scope.sorterList = [
-    {text: "Keine", value: "keine"},
     {text: "Bezeichnung", value: "name"},
     {text: "Bewertung", value: "bewertung"},
     {text: "Rendite", value: "rendite"},
@@ -33,33 +32,67 @@ appController.controller('investitionSearchCtrl',function($scope, $http, apiendp
     {text: "Anzahl der Investoren", value: "anzahl_investoren"}
   ];
 
-
   $scope.sorterOrder = [
     {text: "Aufsteigend", value: true},
     {text: "Absteigend", value: false}
   ];
 
-  $scope.dataSorter = {
-    value: ""
-  };
 
-  $scope.dataOrder = {
-    value: false
-  };
+  $scope.search = function(kategorienFilter, sorterList, sorterOrder) {
 
+    var kategorie = selectFormatter.formatSelect(kategorienFilter.text);
+    var sorterText = selectFormatter.formatSelect(sorterList.text);
+    var orderText = selectFormatter.formatSelect(sorterOrder.text);
 
-  $scope.search = function(kategorienFilter, dataSorter, dataOrder) {
+    var sorterValue = "";
+    if (sorterText !== "Keine") {
+      for (var i = 0; i < $scope.sorterList.length; i++) {
+        if (sorterText === $scope.sorterList[i].text) {
+          sorterValue = $scope.sorterList[i].value;
+          break;
+        }
+      }
+    }
+
+    var orderValue = false;
+    for (var i = 0; i < $scope.sorterOrder.length; i++) {
+      if (orderText === $scope.sorterOrder[i].text) {
+        orderValue = $scope.sorterOrder[i].value;
+        break;
+      }
+    }
+
+    var parameter;
+    if (kategorie === "Alle" && sorterValue === "") {
+      parameter = {
+        ascending: orderValue
+      };
+    } else if (kategorie !== "Alle" && sorterValue === "") {
+      parameter = {
+        kategorie: kategorie,
+        ascending: orderValue
+      };
+    } else if (kategorie !== "Alle" && sorterValue !== "") {
+      parameter = {
+        kategorie: kategorie,
+        orderby: sorterValue,
+        ascending: orderValue
+      };
+    }
 
 
     //kategorienFilter.value= kategorienFilter.value.trim();
-    console.log($scope.sorterList.value);
+    console.log("Kategorie: " + kategorie);
+    console.log("Sorter: " + sorterValue);
+    console.log("Order: " + orderValue);
 
 
-    var parameter = {
-        kategorie: selectFormatter.formatSelect(kategorienFilter.value),
-        orderby: selectFormatter.formatSelect(dataSorter.value),
-        ascending: dataOrder.value
-    };
+
+    /*var parameter = {
+        kategorie: kategorie,
+        orderby: selectFormatter.formatSelect(sorterList.text),
+        ascending: false
+    };*/
 
     $http.post(apiendpoint.url + '/api/smartinsurance/filter', parameter).success(function(response) {
 
