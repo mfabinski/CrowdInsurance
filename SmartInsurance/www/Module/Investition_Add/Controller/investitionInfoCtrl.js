00e059efa79cff211e2206d7bd5b8315f5e12139
lroehrich <s131740@student.dhbw-mannheim.de>
2016-06-11 12:03:57 +0200

    bewertung angepasst in investitionInfo

--- SmartInsurance/www/Module/Investition_Add/Controller/investitionInfoCtrl.js
+++ SmartInsurance/www/Module/Investition_Add/Controller/investitionInfoCtrl.js
@@ -1,20 +1,20 @@
 appController.controller('investitionInfoCtrl',function($scope, $http, $state, $stateParams, moneyParser, moneyFormatter, apiendpoint){
 
     $scope.versicherungID = $stateParams.id;
-    
+
     $scope.noComment = true;
-    
+
      $scope.bewertung = [
                 {count: 0},
                 {count: 0},
                 {count: 0}
     ];
-    
+
     $http.get(apiendpoint.url + '/api/smartinsurance/versicherung/' + $scope.versicherungID).success(function(response) {
          $scope.versicherung = response[0];
     });
 
-     
+
     $http.get(apiendpoint.url + '/api/smartinsurance/schadensfaelle/' + $scope.versicherungID).success(function(response) {
          $scope.schadensfaelle = response;
     });
@@ -25,33 +25,20 @@ appController.controller('investitionInfoCtrl',function($scope, $http, $state, $
             if(angular.isDefined(response[0])) {
                 $scope.investitionBetrag = response[0].suminvestition;
             } else {
-                $scope.investitionBetrag = "0,00 €";   
+                $scope.investitionBetrag = "0,00 €";
             }
         })
-    
+
     $http.get(apiendpoint.url + '/api/smartinsurance/versicherung/' + $scope.versicherungID + '/bewertungen').success(function(response) {
-         $scope.bewertunghelper = response;
-            for (var i=0;i<$scope.bewertunghelper.length;i++){
-                switch ($scope.bewertunghelper[i].bewertung) {
-                    case "keine":
-                        $scope.bewertung[0]=$scope.bewertunghelper[i];
-                        break;
-                    case "daumenHoch":
-                        $scope.bewertung[1]=$scope.bewertunghelper[i];
-                        break;
-                    case "daumenRunter":
-                        $scope.bewertung[2]=$scope.bewertunghelper[i];
-                        break;
-                }
-            }
+        $scope.bewertung = response;
     });
-    
+
     $http.get(apiendpoint.url + '/api/smartinsurance/kommentare/' + $scope.versicherungID).success(function(response) {
             $scope.comments = response;
             $scope.noComment = false;
     });
-    
-      
+
+
     $scope.gesamtSchaden = function () {
         var gesamtSchaden = 0;
         if (angular.isDefined($scope.schadensfaelle)) {
@@ -61,7 +48,7 @@ appController.controller('investitionInfoCtrl',function($scope, $http, $state, $
         };
         return moneyFormatter.formatMoney(gesamtSchaden);
     };
-    
+
 
     $scope.showSchadensfaelle = function() {
         $state.go("app.schadensfaelle", {id: $scope.versicherungID});
