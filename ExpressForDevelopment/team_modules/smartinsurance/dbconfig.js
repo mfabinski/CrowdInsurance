0f9b6cb7f46c0ed44420468585b56ee8108f8c1c
matthias <matthias.fab@gmail.com>
2016-06-13 23:02:51 +0200

    Improve dbconfig module

---
+++ ExpressForDevelopment/team_modules/smartinsurance/dbconfig.js
@@ -0,0 +1,18 @@
+// Retreive credentials
+exports.getCredentials = function() {
+    var credentials;
+    // Passed as commandline parameter?
+    if (process.argv[2] != undefined && (process.argv[2].startsWith('postgres'))) {
+        credentials = process.argv[2];
+        return credentials;
+    }
+    // Configured locally with dbconfig.js file?
+    try {
+        var dbconfig = require('../../node_modules/dbconfig.js');
+        credentials = dbconfig.url;
+        return credentials;
+    } catch(err) {}
+    // Configured with environment variable?
+    credentials = 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASENAME + ((process.env.DB_SSL=="true")?'?ssl=true':'');
+    return credentials;
+};
