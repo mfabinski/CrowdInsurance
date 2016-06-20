51274ba9e2cd62cd09fbc0e46a9bfa7321336bce
Matthias Fabinski <matthias.fab@gmail.com>
2016-06-20 22:35:48 +0200

    Harmonisierung mit dem Backend

--- ExpressForDevelopment/team_modules/smartinsurance/dbconfig.js
+++ ExpressForDevelopment/team_modules/smartinsurance/dbconfig.js
@@ -12,6 +12,12 @@ exports.getCredentials = function() {
         credentials = dbconfig.url;
         return credentials;
     } catch(err) {}
+    // Configured by SmartBackend?
+    try {
+        var dbconfig = require('../../node_modules/smartbackendHelper/lib/database.js');
+        credentials = dbconfig.smartinsurance;
+        return credentials;
+    } catch(err) {}
     // Configured with environment variable?
     credentials = 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASENAME + ((process.env.DB_SSL=="true")?'?ssl=true':'');
     return credentials;
