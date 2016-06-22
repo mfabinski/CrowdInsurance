268411e9b73145c285cdd12459e513bde25379f5
dhentschel06 <s130861@student.dhbw-mannheim.de>
2016-06-22 12:59:42 +0200

    fix test

--- ExpressForDevelopment/team_modules/smartinsurance/validator.js
+++ ExpressForDevelopment/team_modules/smartinsurance/validator.js
@@ -188,7 +188,7 @@ exports.parameterKommentar = function(req,res,next){
     if(text != undefined && versicherungID != undefined && text.length > 0 && internal.isNumber(versicherungID)) {
       tdg.selectVersicherung(versicherungID,
           function(data){
-              if(data.length == 1){
+              if(data[1] != null){
                   logger.consoleInfo('Versicherung ID ' + versicherungID + ' soll scheinbar existieren.');
                   next();
               } else{
