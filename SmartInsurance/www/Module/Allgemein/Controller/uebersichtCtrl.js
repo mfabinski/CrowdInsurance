ae62a4afd265cd0992c5a5beec9b12623a6d5aab
ybart15 <s130177@student.dhbw-mannheim.de>
2016-06-23 19:27:13 +0200

    Charts undso

--- SmartInsurance/www/Module/Allgemein/Controller/uebersichtCtrl.js
+++ SmartInsurance/www/Module/Allgemein/Controller/uebersichtCtrl.js
@@ -11,14 +11,349 @@ appController.controller('uebersichtCtrl', function($scope, $http, $state, money
     $http.get(apiendpoint.url + '/api/smartinsurance/versicherung').success(function(response) {
         $scope.versicherungen = response;
         $scope.noVersicherung = false;
+        
+        $scope.chartObject = {
+  "type": "PieChart",
+  "displayed": false,
+  "data": {
+    "cols": [
+      {
+        "id": "kategorie",
+        "label": "Kategorie",
+        "type": "string",
+        "p": {}
+      },
+      {
+        "id": "anzahl",
+        "label": "Anzahl",
+        "type": "number",
+        "p": {}
+      },
+    ],
+    "rows": [
+      {
+        "c": [
+          {
+            "v": "Autos"
+          },
+          {
+            "v": $scope.versicherungen.length
+              
+            
+          },
+        ]
+      },
+      {
+        "c": [
+          {
+            "v": "Schiff"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+      {
+        "c": [
+          {
+            "v": "Flugzeug"
+          },
+          {
+            "v": 24
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Haus"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Küchengeräte"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Möbel"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Maschinen"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Uhr"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Sonstiges"
+          },
+          {
+            "v": 13
+          },
+        ]
+      }
+    ]
+  },
+  "options": {
+    "title": "Versicherungsangebote pro Kategorie",
+    "isStacked": "true",
+    "fill": 20,
+    "displayExactValues": true,
+    "vAxis": {
+      "title": "Sales unit",
+      "gridlines": {
+        "count": 10
+      }
+    },
+    "hAxis": {
+      "title": "Date"
+    }
+  },
+  "formatters": {}
+}
+        
+        
 
+    
     });
 
+    
     $http.get(apiendpoint.url + '/api/smartinsurance/investition').success(function(response) {
         $scope.investitionen = response;
         $scope.noInvestition = false;
-    });
+        
+         $scope.chartObject2 = {
+  "type": "PieChart",
+  "displayed": false,
+  "data": {
+    "cols": [
+      {
+        "id": "kategorie",
+        "label": "Kategorie",
+        "type": "string",
+        "p": {}
+      },
+      {
+        "id": "anzahl",
+        "label": "Anzahl",
+        "type": "number",
+        "p": {}
+      },
+    ],
+    "rows": [
+      {
+        "c": [
+          {
+            "v": "Autos"
+          },
+          {
+            "v": $scope.investitionen.length
+                   
+          },
+        ]
+      },
+      {
+        "c": [
+          {
+            "v": "Schiff"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+      {
+        "c": [
+          {
+            "v": "Flugzeug"
+          },
+          {
+            "v": 24
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Haus"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Küchengeräte"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Möbel"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Maschinen"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Uhr"
+          },
+          {
+            "v": 13
+          },
+        ]
+      },
+              {
+        "c": [
+          {
+            "v": "Sonstiges"
+          },
+          {
+            "v": 13
+          },
+        ]
+      }
+    ]
+  },
+  "options": {
+    "title": "Investitionen pro Kategorie",
+    "isStacked": "true",
+    "fill": 20,
+    "displayExactValues": true,
+    "vAxis": {
+      "title": "Sales unit",
+      "gridlines": {
+        "count": 10
+      }
+    },
+    "hAxis": {
+      "title": "Date"
+    }
+  },
+  "formatters": {}
+         }
+         
+         
+                                              $scope.chartObject3 = {
+  "type": "ColumnChart",
+  "displayed": false,
+  "data": {
+    "cols": [
+      {
+        "id": "art",
+        "label": "",
+        "type": "string",
+        "p": {}
+      },
+      {
+        "id": "summe",
+        "label": "Geld",
+        "type": "number",
+        "p": {}
+      },
+    ],
+    "rows": [
+      {
+        "c": [
+          {
+            "v": "Versicherungshöhe"
+          },
+          {
+            "v": $scope.versicherungengesamt()
+                   
+          },
+        ]
+      },
+      {
+        "c": [
+          {
+            "v": "Investitionshöhe"
+          },
+          {
+            "v": $scope.investhoehegesamt()
+          },
+        ]
+      }
+      
+    ]
+  },
+  "options": {
+    "title": "Versicherungshöhe im Vergleich zur Investitionshöhe",
+    "isStacked": "true",
+    "fill": 20,
+    "displayExactValues": true,
+    "vAxis": {
+      "title": "€",
+      "gridlines": {
+        "count": 10
+      }
+    },
+    "hAxis": {
+      "title": "Art"
+    }
+  },
+  "formatters": {}
+}
+         
+                     
 
+                 
+        
+    });
+    
+    
+
+    
     $scope.versicherungShow = function(id) {
         $state.go("app.versicherungDetail",{id: id});
     };
@@ -50,7 +385,11 @@ appController.controller('uebersichtCtrl', function($scope, $http, $state, money
         for (var i = 0; i < $scope.investitionen.length; i++){
             gesamtbetrag += moneyParser.moneyparsen($scope.investitionen[i].investitionshoehe);
         }
+        
         return moneyFormatter.formatMoney(gesamtbetrag);
+        
+
+                                     
     };
 
     $scope.renditemax = function() {
@@ -74,6 +413,12 @@ appController.controller('uebersichtCtrl', function($scope, $http, $state, money
 
         return moneyFormatter.formatMoney(monatsbeitrag * investitionshoehe /versicherungshoehe);
     };
+    
+    
+
+
+    
+
 
 
 });
