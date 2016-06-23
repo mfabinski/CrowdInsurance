81d8ff5b9c8550ba7777333b7905d5c277170373
Jannik <SAP Laptop>
2016-06-23 17:17:30 +0200

    signup prüfungen

--- SmartInsurance/www/Module/Allgemein/Controller/signupCtrl.js
+++ SmartInsurance/www/Module/Allgemein/Controller/signupCtrl.js
@@ -2,23 +2,36 @@ appController.controller('signupCtrl', function($scope,  $http, $ionicPopup, $st
     $scope.form = {};
     $scope.token;
     $scope.submitted = false;
-
+    $scope.samePw = false;
 
     $scope.isInvalid = function(field){
         console.log(field);
         return field.$error.required && (field.$touched || $scope.submitted);
     };
 
-
-  /*  $scope.isNaN = function(field) {
-        return field.$error.pattern && (field.$touched || $scope.submitted);
+    $scope.isSamePw = function(field){
+        console.log(field);
+        if ($scope.form.passwort == $scope.form.passwortWiederholung) {
+            $scope.samePw = true;
+            return false;
+        }
+        else {
+            $scope.samePw = false;
+            return true;
+        }
     };
-*/
+
+    $scope.isName = function(field) {
+
+
+        return truefield.$error.pattern && (field.$touched || $scope.submitted);
+    };
+
 
 
     $scope.signup = function (signupform) {
 
-        if (signupform.$valid) {
+        if (signupform.$valid && $scope.samePw) {
             var data = {
                 email: $scope.form.email,
                 password: $scope.form.passwort
@@ -29,7 +42,25 @@ appController.controller('signupCtrl', function($scope,  $http, $ionicPopup, $st
                     console.log("erfolgreich");
                     $scope.token = response.data;
                     $http.defaults.headers.common['Authorization'] = "Bearer " + response.data;
+                    url = apiendpoint.url + "/api/smartinsurance/profil";
+                    data = {
+                        personID: "TODO",
+                        name: $scope.form.nachname,
+                        prename: $scope.form.vorname,
+                        email: $scope.form.email,
+                        iban : $scope.form.iban,
+                        bic : $scope.form.bic,
+                        bankinstitut : $scope.form.bank
 
+                    };
+                    http.post(url, data).then(function(response)
+                    {
+                        console.log("Profil erstellt");
+                    },
+                    function(error){
+                    console.log("Error beim Erstellen des Smartinsurance-Profils");
+                    }
+                    )
 
                     var alertPopup = $ionicPopup.alert({
                         title: 'Registrierung',
@@ -41,7 +72,7 @@ appController.controller('signupCtrl', function($scope,  $http, $ionicPopup, $st
                         console.log('Thank you for not eating my delicious ice cream cone');
                         $state.go('app.uebersicht')
                     });
-                    
+
                 }, function (error) {
                     $ionicPopup.alert({
                         title: 'Registrierung',
@@ -52,7 +83,6 @@ appController.controller('signupCtrl', function($scope,  $http, $ionicPopup, $st
 
 
         }
-
 
 /*                 $http({
                  method: "GET",
