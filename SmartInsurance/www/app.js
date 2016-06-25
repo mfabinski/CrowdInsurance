97934c73fe3e5f86e58dd93dbfa8df1676d4383f
Jannik <SAP Laptop>
2016-06-25 10:38:47 +0200

    app.js anpassungen zum login

--- SmartInsurance/www/app.js
+++ SmartInsurance/www/app.js
@@ -259,6 +259,26 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
             }
         })
 
+        .state('app.login', {
+            url: '/login',
+            views: {
+                'menuContent': {
+                    templateUrl: 'Module/Allgemein/Views/login.html',
+                    controller: 'loginCtrl'
+                }
+            }
+        })
+
+        .state('app.signup', {
+            url: '/signup',
+            views: {
+                'menuContent': {
+                    templateUrl: 'Module/Allgemein/Views/signup.html',
+                    controller: 'signupCtrl'
+                }
+            }
+        })
+
         .state('app.faq', {
             url: '/faq',
             views: {
