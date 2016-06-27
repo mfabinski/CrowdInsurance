f5f46bb758c34cfd17a9bafdbb1181c8805ec53f
ybart15 <s130177@student.dhbw-mannheim.de>
2016-06-27 16:36:51 +0200

    Rebase Konflikte geloest

--- SmartInsurance/www/Module/Sonstiges/Controller/profilBearbeitenCtrl.js
+++ SmartInsurance/www/Module/Sonstiges/Controller/profilBearbeitenCtrl.js
@@ -1,18 +1,68 @@
-appController.controller('profilBearbeitenCtrl',function($scope, $http, $state, $stateParams, apiendpoint){
+appController.controller('profilBearbeitenCtrl',function($scope, $http, $state, $stateParams, apiendpoint, $ionicPopup){
 
     
     
     $http.get(apiendpoint.url + '/api/smartinsurance/profil').success(function(response) {
-         $scope.profil = response[0];
-         $scope.prename = response[0].prename;
-         $scope.name = response[0].name;
-         $scope.email = response[0].email;
-         $scope.iban = response[0].iban;
-         $scope.email = response[0].bic;
+        $scope.profil = response[0];      
+        $scope.profilNeu = {
+            personID: $scope.profil.id,
+            name: $scope.profil.name,
+            prename: $scope.profil.prename,
+            email: $scope.profil.email,
+            iban: $scope.profil.iban,
+            bic: $scope.profil.bic,
+            birthday: $scope.profil.birthday
+        }
+    }).error(function(error, status) {
+        CacheHistoryReseter.reset();
+        $state.go("app.error", {error: {message: error, status: status}});
     });
     
 
     
+
+
+        $scope.checkIBAN = /[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}/;
+
+         $scope.funcIBAN = function(field){
+            return field.$error.pattern && (field.$touched || $scope.submitted);
+        }
+
+        $scope.isInvalid = function(field){
+            return field.$error.required && (field.$touched || $scope.submitted);
+        };
+
+
+
+
+    $scope.profilBearbeiten = function(form) {
+
+        $scope.submitted = true;
+
+        if (form.$valid) {
+
+
+                $ionicPopup.show({
+                    title: 'Bestätigung',
+                    template: 'Wollen Sie die Änderung speichern?',
+                    buttons: [
+                      { text: 'Cancel' },
+                      {
+                        text: '<b>Speichern</b>',
+                        type: 'button-positive',
+                        onTap: function(e) {
+                            // Schnittstelle ist noch nicht implementiert
+                            $http.post(apiendpoint.url + '/api/smartinsurance/profil', $scope.profilNeu).then(function(data) {
+                                CacheHistoryReseter.reset();
+                                $state.go('app.uebersicht');
+                            });
+                        }
+                      }
+                    ]
+                });
+        }
+        }
+
 
 
 });
--- SmartInsurance/www/Module/Sonstiges/Controller/profilFremdCtrl.js
+++ SmartInsurance/www/Module/Sonstiges/Controller/profilFremdCtrl.js
@@ -4,18 +4,37 @@ appController.controller('profilFremdCtrl',function($scope, $http, $state, $stat
     
     $http.get(apiendpoint.url + '/api/smartinsurance/profil').success(function(response) {
          $scope.profil = response[0];
-         $scope.prename = response[0].prename;
-         $scope.name = response[0].name;
-         $scope.email = response[0].email;
-         $scope.iban = response[0].iban;
-         $scope.email = response[0].bic;
     });
     
     $scope.versicherungen= [];
     
         $http.get(apiendpoint.url + '/api/smartinsurance/versicherung').success(function(response) {
          $scope.versicherungen = response;
-
+            
+            $scope.gespos = 0;
+            $scope.gesneg = 0;
+          for (var i = 0; i < $scope.versicherungen.length; i++){
+            $http.get(apiendpoint.url + '/api/smartinsurance/versicherung/' + $scope.versicherungen[i].id + '/bewertungen').success(function(response) {
+                if(angular.isDefined(response[0])) {
+                    $scope.bewertung = response;
+                    $scope.pos = bewertung[0];
+                    $scope.neg = bewertung[1];
+                    
+                    $scope.gespos += $scope.pos;
+                     $scope.gesneg += $scope.neg;
+                }
+            });
+        }
+        $scope.avg = $scope.gespos - $scope.gesneg;
+            
+             
+    });
+    
+    
+    $http.get(apiendpoint.url + '/api/smartinsurance/versicherung/' + $scope.versicherungId + '/bewertungen').success(function(response) {
+         if(angular.isDefined(response[0])) {
+            $scope.bewertung = response;
+        }
     });
 
         $scope.avgBew = function() {
