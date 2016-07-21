4912bcf03a5b86786a4a5701fbe9f7c1639f7052
lroehrich <s131740@student.dhbw-mannheim.de>
2016-07-21 17:16:54 +0200

    link 2

--- SmartInsurance/www/Module/Sonstiges/Controller/profilFremdCtrl.js
+++ SmartInsurance/www/Module/Sonstiges/Controller/profilFremdCtrl.js
@@ -1,53 +1,60 @@
-appController.controller('profilFremdCtrl',function($scope, $http, $state, $stateParams, apiendpoint){
+appController.controller('profilFremdCtrl',function($scope, $http, $state, $stateParams, apiendpoint, CacheHistoryReseter){
 
-    $scope.profilID = $stateParams.investor.personID;
-    console.log($scope.profilID);
+   if ($stateParams.investor == null) {
+        CacheHistoryReseter.reset();
+        $state.go("app.error", {error: {message: "Fehler beim erneuten Laden der Seite.", status: "404"}});
+    } else {
 
-    $http.get(apiendpoint.url + '/api/smartinsurance/profil/' + $scope.profilID).success(function(response) {
-         $scope.profil = response[0];
-    });
+        $scope.profilID = $stateParams.investor.personID;
+        console.log($scope.profilID);
 
-    $scope.versicherungen= [];
-    $scope.bewertungen = [];
-
-        $http.get(apiendpoint.url + '/api/smartinsurance/versicherung/person/' + $scope.profilID).success(function(response) {
-            $scope.versicherungen = response;
-
-            $scope.avgBewertung = 0;
-
-            var getBewertung = function(i){
-              $http.get(apiendpoint.url + '/api/smartinsurance/versicherung' + $scope.versicherungen[i].id + '/bewertungen').success(function(response) {
-                    if(angular.isDefined(response[0])) {
-
-                        $scope.bewertungen.push(response);
-
-                        $scope.pos = Number($scope.bewertungen[i][1].count);
-                        $scope.neg = Number($scope.bewertungen[i][2].count);
-
-                        $scope.bewertung = $scope.pos - $scope.neg;
-
-                        $scope.avgBewertung +=  $scope.bewertung;
-
-                        $scope.versicherungen[i].bewertung = $scope.bewertung;
-                    } else {
-                        $scope.bewertungen.push($scope.bewertung = [
-                            {count: 0},
-                            {count: 0},
-                            {count: 0}
-                        ]);
-                        $scope.versicherungen[i].bewertung = 0;
-                    }
-                });
-            }
-
-            for (var i = 0; i < $scope.versicherungen.length; i++){
-              getBewertung(i);
-            }
+        $http.get(apiendpoint.url + '/api/smartinsurance/profil/' + $scope.profilID).success(function(response) {
+             $scope.profil = response[0];
         });
 
-     $scope.versicherungShow = function(id) {
-        $state.go("app.investitionInfo",{id: id});
-    };
+        $scope.versicherungen= [];
+        $scope.bewertungen = [];
+        $scope.avgBewertung = 0;
+        $scope.noVersicherung = true;
+
+            $http.get(apiendpoint.url + '/api/smartinsurance/versicherung/person/' + $scope.profilID).success(function(response) {
+                $scope.versicherungen = response;
+                $scope.noVersicherung = false;
+
+                var getBewertung = function(i){
+                  $http.get(apiendpoint.url + '/api/smartinsurance/versicherung' + $scope.versicherungen[i].id + '/bewertungen').success(function(response) {
+                        if(angular.isDefined(response[0])) {
+
+                            $scope.bewertungen.push(response);
+
+                            $scope.pos = Number($scope.bewertungen[i][1].count);
+                            $scope.neg = Number($scope.bewertungen[i][2].count);
+
+                            $scope.bewertung = $scope.pos - $scope.neg;
+
+                            $scope.avgBewertung +=  $scope.bewertung;
+
+                            $scope.versicherungen[i].bewertung = $scope.bewertung;
+                        } else {
+                            $scope.bewertungen.push($scope.bewertung = [
+                                {count: 0},
+                                {count: 0},
+                                {count: 0}
+                            ]);
+                            $scope.versicherungen[i].bewertung = 0;
+                        }
+                    });
+                }
+
+                for (var i = 0; i < $scope.versicherungen.length; i++){
+                  getBewertung(i);
+                }
+            });
+
+         $scope.versicherungShow = function(id) {
+            $state.go("app.investitionInfo",{id: id});
+        };
+    }
 
 
 });
