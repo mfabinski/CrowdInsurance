27d8a40db0b7f52ac3976d3bd50f0a7cc36068c2
matthias <matthias.fab@gmail.com>
2016-07-26 21:46:07 +0200

    Login with our hosted smartbackend at smartbackend.cloudf.de

--- SmartInsurance/www/Module/apiendpoint.js
+++ SmartInsurance/www/Module/apiendpoint.js
@@ -1,9 +1,7 @@
 angular.module("smartInsurance").factory('apiendpoint', function() {
     var service={};
     service.url = "http://localhost:3000";
-//    service.url = "http://crowdinsurance.cloudf.de";
-    service.backend = "https://sb.pftclan.de:546"
-  //  service.backend = "http://smartbackend.cloudf.de"
+    service.backend = "http://smartbackend.cloudf.de"
 
     return service;
 });
